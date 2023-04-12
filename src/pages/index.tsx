963505424f51326a829f44c6dcf707a9e87c2b0f
JayBrown92 <49319765+JayBrown92@users.noreply.github.com>
2023-04-12 01:08:41 +0100

    features and about section complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,47 +50,52 @@ export default function Home() {
         className="hero-image"
       />
       <section id="features-section" className="features">
-        <div>
-          <p>1</p>
-          <h2>Actionable insights</h2>{" "}
-          <p>
-            Optimize your products, improve customer satisfaction and stay ahead
-            of the competition with our product data analytics.
-          </p>
-        </div>
-        <div>
-          <p>2</p> <h2>Data-driven decisions</h2>{" "}
-          <p>
-            {" "}
-            Make data-driven decisions with our product data analytics. Our
-            AI-generated reports help you unlock insights hidden in your product
-            data.{" "}
-          </p>
-        </div>
-        <div>
-          <p>3</p>
-          <h2>Always affordable</h2>
-          <p>
-            Always affordable pricing that scales with your business. Get
-            top-quality product data analytics services without hidden costs or
-            unexpected fees.
-          </p>
+        <div className="container">
+          <div className="content">
+            <span>1</span>
+            <h2>Actionable insights</h2>{" "}
+            <p>
+              Optimize your products, improve customer satisfaction and stay
+              ahead of the competition with our product data analytics.
+            </p>
+          </div>
+          <div className="content">
+            <span>2</span> <h2>Data-driven decisions</h2>{" "}
+            <p>
+              {" "}
+              Make data-driven decisions with our product data analytics. Our
+              AI-generated reports help you unlock insights hidden in your
+              product data.{" "}
+            </p>
+          </div>
+          <div className="content">
+            <span>3</span>
+            <h2>Always affordable</h2>
+            <p>
+              Always affordable pricing that scales with your business. Get
+              top-quality product data analytics services without hidden costs
+              or unexpected fees.
+            </p>
+          </div>
         </div>
       </section>
-      <section id="about-section">
-        <Image
-          src={FounderImage}
-          alt="An image of WorkIt founder, Louis Graham"
-        />{" "}
-        <div>
-          <h2>Be the first to test</h2>
-          <p>
-            Hi, I&apos;m Louis Graham, the founder of the company. Book a demo
-            call with me to become a beta tester for our app and kickstart your
-            company. Apply for access below and I&apos;ll be in touch to
-            schedule a call.
-          </p>
-          <button>Apply for access</button>
+      <section id="about-section" className="about">
+        <div className="container">
+          <Image
+            src={FounderImage}
+            width={281}
+            alt="An image of WorkIt founder, Louis Graham"
+          />{" "}
+          <div className="content">
+            <h2>Be the first to test</h2>
+            <p>
+              Hi, I&apos;m Louis Graham, the founder of the company. Book a demo
+              call with me to become a beta tester for our app and kickstart
+              your company. Apply for access below and I&apos;ll be in touch to
+              schedule a call.
+            </p>
+            <button className="btn-primary">Apply for access</button>
+          </div>
         </div>
       </section>
       <footer>
